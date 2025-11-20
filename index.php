fa4f2c88a9e63ad4510de43641929cbeccec055d
codecuatai <vtai2805@gmail.com>
2025-11-20 22:54:18 +0700

    final PHT Chương 3

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="vi">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trang chủ</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <h1>大家好你们！</h1>
 
@@ -13,10 +15,12 @@
         <h2>Bài tập phiếu bài tập</h2>
         <ul>
             <li><a href="./Code_PHT/Chuong1-2/chapter2.php">Code chương 2</a></li>
+            <li><a href="./Code_PHT/Chuong3/login.html">Code chương 3</a></li>
         </ul>
     </nav>
 
-    <br><br><hr><br><br>
+    <br><br>
+    <hr><br><br>
 
     <nav>
         <h2>这是我的练词</h2>
@@ -28,5 +32,5 @@
     </nav>
 
 </body>
-</html>
 
+</html>
